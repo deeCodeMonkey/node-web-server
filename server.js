91d191b5aa4f4ec6e84200afc8b7455decb0f7f1
deeCodeMonkey <deidra108@gmail.com>
2017-10-09 15:24:48 -0600

    add projects page

--- server.js
+++ server.js
@@ -24,12 +24,12 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    res.render('maintenance.hbs', {
-        pageTitle: 'Maintenance',
-        message: 'Please check back later!'
-    });
-});
+//app.use((req, res, next) => {
+//    res.render('maintenance.hbs', {
+//        pageTitle: 'Maintenance',
+//        message: 'Please check back later!'
+//    });
+//});
 
 app.use(express.static(__dirname + '/public')); 
 
@@ -51,6 +51,12 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page',
+    });
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
     });
 });
 
